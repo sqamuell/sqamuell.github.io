b1aef6b53a212d4b33dff096f2c61541aa039c05
sqamuell <quietmilkshake@gmail.com>
2022-06-07 22:25:32 -0400

    moda

--- modal.js
+++ modal.js
@@ -18,7 +18,7 @@ var modal = document.getElementById("myModal");
 var modalImg = document.getElementById("img01");
 var captionText = document.getElementById("caption");
 img.onclick = function(){
-modal.style.display = "block";
+modal.style.display = "flex";
 modalImg.src = this.src;
 captionText.innerHTML = this.alt;
 }
