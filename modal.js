a3250c93e7cbca0fb70dfc38f1577b3f4de2ba03
sqamuell <quietmilkshake@gmail.com>
2022-06-07 22:29:37 -0400

    Update modal.js

--- modal.js
+++ modal.js
@@ -18,7 +18,7 @@ var modal = document.getElementById("myModal");
 var modalImg = document.getElementById("img01");
 var captionText = document.getElementById("caption");
 img.onclick = function(){
-modal.style.display = "flex";
+modal.style.display = "block";
 modalImg.src = this.src;
 captionText.innerHTML = this.alt;
 }
